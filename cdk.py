5cbed3ccba1712483f099ec8bd8f7b6e7f2d9107
Anthony Lukach <anthonylukach@gmail.com>
2021-01-21 11:36:49 -0700

    Simplify

--- cdk.py
+++ cdk.py
@@ -6,25 +6,6 @@ from cdk.stack import AppStack
 
 
 app = core.App()
-basename = "lambda-monorepo-example"
-STAGE = os.environ.get("STAGE", "dev")
-
-AppStack(
-    app,
-    f"{basename}-{STAGE}",
-    env=core.Environment(
-        account=os.environ["CDK_DEFAULT_ACCOUNT"],
-        region=os.environ["CDK_DEFAULT_REGION"],
-    ),
-)
-
-# Tag infrastructure
-for key, value in {
-    "Project": "monorepo-example",
-    "Owner": os.environ.get("OWNER"),
-    "Client": "example-client",
-    "Stack": STAGE,
-}.items():
-    core.Tags.of(app).add(key, value, apply_to_launched_instances=True)
+AppStack(app, "lambda-monorepo-example")
 
 app.synth()

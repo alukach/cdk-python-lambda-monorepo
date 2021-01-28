261b6e73a97777ac50def23cd7377473ca35e545
Ciaran Evans <ciaran@developmentseed.org>
2021-01-28 14:52:53 +0000

    Cleanup db_utils setup.py

--- layers/db-utils/setup.py
+++ layers/db-utils/setup.py
@@ -1,23 +1,9 @@
 import setuptools
 
-# with open("README.md", "r", encoding="utf-8") as fh:
-#     long_description = fh.read()
-
 setuptools.setup(
     name="db-utils",
     version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
-    # long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
     packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.6",
+    python_requires=">=3.8",
     install_requires=["sqlalchemy>=1.3.22"],
 )
